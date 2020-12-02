9f444b29ff731eb6ed4f4a4f278347018e8c776f
arfpchatelot <fchatelot@yahoo.fr>
2020-12-02 12:17:15 +0100

    finalisation tri à bulle fichier program

--- tri_a_bulle_tableau/Program.cs
+++ tri_a_bulle_tableau/Program.cs
@@ -10,7 +10,7 @@ namespace tri_a_bulle_tableau
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Combien d'éléments souhaitez-vous dans le tableau?");
+            Console.WriteLine("Combien d'éléments souhaitez-vous dans le tableau ci-présent?");
             int nbrecase = int.Parse(Console.ReadLine());
             int[] tabNombre = new int[nbrecase];
             for (int i = 0; i < nbrecase; i++)
@@ -22,11 +22,15 @@ namespace tri_a_bulle_tableau
 
 
             ManipulationTableau.AfficherTab(tabNombre);
-
+            Console.WriteLine("Appuyer sur une touche pour trier le tableau!!");
+            Console.ReadKey();
+           tabNombre= ManipulationTableau.Trierbuller(tabNombre);
+            ManipulationTableau.AfficherTab(tabNombre);
             Console.ReadKey();
 
-        }
 
+        }
+       
     }
     class ManipulationTableau
     {
@@ -39,7 +43,40 @@ namespace tri_a_bulle_tableau
             }
             Console.Write(" ] \n");
         }
-            
-            
+
+        public static int[] Trierbuller( int[] _tab)
+        {
+            bool trier;
+            int temp;
+            do
+            {
+                trier = true;
+                for (int i = 0; i < _tab.Length - 1; i++)
+                {
+
+                    if (_tab[i] > _tab[i + 1])
+                    {
+
+                        trier = false;
+                        temp = _tab[i];
+                        _tab[i] = _tab[i + 1];
+                        _tab[i + 1] = temp;
+                    }
+                }
+            } while (trier == false);
+
+
+          
+
+            return  _tab;
+
+        }
+
+        public static int increment(int _n)
+        {
+            _n += 2;
+            Console.WriteLine("valeur incrémenté :"+_n);
+            return _n;
+        }
     }
 }
