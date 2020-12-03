9b44df68155b729a15e22b974fc3c250a360208a
arfpchatelot <fchatelot@yahoo.fr>
2020-12-03 16:32:45 +0100

    Ajout projet majorité avec DateTime et bibliothèque de classe

---
+++ Controlesaisies/ControleInterface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlesaisies
+{
+    public class ControleInterface
+    {
+        public static bool ControleMajorite( DateTime _dateNaissance)
+        {
+            bool majeur;
+
+            DateTime dateduJour = DateTime.Today;
+            TimeSpan duree = dateduJour - _dateNaissance;
+            double nbJour1 = duree.TotalDays;// retourne un double
+            double nbJour2 = duree.Days;// retourne un int nombre de jour entier arrondi
+
+            //Console.Write("nombre de jour 1: {0:#,###.00} et nombre de jour 2 : {1:#,###.00} \n", nbJour1, nbJour2);
+            double nombreAnnee = Math.Floor((double)nbJour2 / 365.2425);
+            if (nombreAnnee >= 18)
+            {
+                majeur = true;
+            }
+            else
+            {
+                majeur = false;
+            }
+
+            return majeur;
+
+        }
+
+
+        public static bool VerifMajorite(DateTime _dateNaissance)
+        {
+
+            DateTime datejour = DateTime.Today;
+            DateTime jourNaissance = new DateTime((datejour.Year - 18), datejour.Month, datejour.Day);
+            //    d2                       d3
+            if (jourNaissance.CompareTo(_dateNaissance) == 1)
+            {
+                return true;
+            }
+            else if (jourNaissance.CompareTo(_dateNaissance) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
---
+++ controle_majorite/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controlesaisies;
+namespace controle_majorite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Quelle est votre date de naissance? dd/mm/yyyy");
+            string[] elementdate = (Console.ReadLine()).Split('/');
+           
+            DateTime datenaissance = new DateTime(int.Parse(elementdate[2]), int.Parse(elementdate[1]), int.Parse(elementdate[0]));
+
+            if (ControleInterface.VerifMajorite(datenaissance))
+            {
+                Console.WriteLine("Vous êtes bien majeur!!");
+            }
+            else
+            {
+                Console.WriteLine("Vous n'avez pas l'age de la majorité requis!!<18");
+            }
+
+            Console.ReadKey();
+
+        }
+
+        //static bool VerifMajorite(DateTime _dateNaissance)
+        //{
+         
+        //    DateTime datejour = DateTime.Today;
+        //    DateTime jourNaissance = new DateTime((datejour.Year-18),datejour.Month,datejour.Day);
+        //    //    d2                       d3
+        //    if (jourNaissance.CompareTo(_dateNaissance)==1)
+        //    {
+        //        return true;
+        //    }
+        //    else if (jourNaissance.CompareTo(_dateNaissance) == 0) 
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+                    
+         
+        //}
+    }
+}
