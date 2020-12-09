f86d2b4bdbdca657a79e7a366037dc45e35f81de
arfpchatelot <fchatelot@yahoo.fr>
2020-12-09 21:04:43 +0100

    Amélioration du projet simulateur de crédit
    Ajout de la fonction statique affichertableau_d'amortissement du prêt dans la bibliothèque de classe lefinancier utilisation appli console

--- Lefinancier/credit.cs
+++ Lefinancier/credit.cs
@@ -8,6 +8,27 @@ namespace Lefinancier
 {
     public class credit
     {
+        public static void AfficherTableau_Amrt(double _capital, double _interet, double _NbMois, double _mensualite)
+        {
+            double CapitalRestantDu=_capital;
+            double PartInteret_Mens;
+            double PartCapital_Mens;
+            for (int i = 1; i <= _NbMois; i++)
+            {
+                PartInteret_Mens = CapitalRestantDu * _interet;
+                PartCapital_Mens = _mensualite - PartInteret_Mens;
+
+                CapitalRestantDu = CapitalRestantDu - PartCapital_Mens;
+
+                Console.Write("mois num:{0} / part intérêt : {1:#.0 } / part Kal : {2:#.0} / capital restant du :{3:#.0}/ mensualité {4} \n", i, PartInteret_Mens, PartCapital_Mens, CapitalRestantDu,_mensualite);
+            }
+
+            
+
+
+        }
+
+
 
         public static double CalculMensualite(double _Capital, double _Interet, double _NbMois)
         {
